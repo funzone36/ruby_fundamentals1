265e9bcb7e225ce0a8861cea38abf0b465386fd0
funzone36 <funzone36@gmail.com>
2016-05-31 13:32:07 -0400

    finished exercise 1

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,31 @@
 puts 2
 puts 3
 puts 2 + 3
+
+puts 'Hello world'
+puts "Hello world"
+
+puts "Betty's pie shop"
+puts 'Betty\'s pie shop'
+
+puts "Ada Lovelace lived for #{1852 - 1815} years."
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
+
+(1 < 3) && (3 < 5)
+# true
+
+(1 > 1) && (2 > 2)
+# false
+
+(1 == 2) || (2 < 3)
+# true
+
+(1 != 2) || (2 < 3)
+# true
+
+(1 == 2) || (2 == 3)
+#false
