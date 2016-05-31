6cf05ecc45cce3274ec02ad6f2c32b97d626bd94
funzone36 <funzone36@gmail.com>
2016-05-31 14:10:16 -0400

    Finished exercise 4

--- exercise4.rb
+++ exercise4.rb
@@ -1,9 +1,13 @@
 one_to_hundred = (1..100)
 
 one_to_hundred.each do |number|
-  if (number%3) == 0
+  if (number%3) == 0 && (number%5) == 0 then
+    puts "Bitmaker"
+  elsif (number%3) == 0
     puts "bit"
-  else
+  elsif (number%5) == 0
     puts "maker"
+  else
+    puts number
   end
-end 
+end
